8711b8af131c672326f0cbc013b7e18cf9c53d6d
Luddo183 <93882319+Luddo183@users.noreply.github.com>
2024-12-17 09:28:39 -0500

    updating to 2025

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "AdvantageKit-Swerve";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 6;
+  public static final String GIT_SHA = "0438e07d893188fdac02ad20907908deed76ce9e";
+  public static final String GIT_DATE = "2024-12-02 15:22:45 EST";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2024-12-06 12:43:09 EST";
+  public static final long BUILD_UNIX_TIME = 1733506989004L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants(){}
+}
